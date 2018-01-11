56dccc94ea64e7d71b92c89f1d764d7688a9df8c
Marta <martadipuccio@me.com>
2018-01-11 15:55:27 +0100

    form validation added
    email, password

--- frontend_test.js
+++ frontend_test.js
@@ -1,6 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('test_input').addEventListener("submit", function(e){
-    e.preventDefault();
-    console.log( checkUsername(e.target[0].value) );
+document.addEventListener("DOMContentLoaded", function() {   // wait until i reload the page and everything is executed
+  document.getElementById('test_input').addEventListener("submit", function(e){  // select the elem in the  document                                                                                         document 
+    e.preventDefault();     // we handle the action, preventing a default action from js, default action should not be taken as it normally would be.
+    console.log(e);
+    // console.log(this) // this here refers to the form
+    // console.log(this.elements[0].value)  
+    // console.log( checkUsername(e.target[0].value) );
+    // console.log( checkEmail(e.target[1].value) );
+    // console.log( checkPassword(e.target[2].value) );
+    if (checkUsername(e.target[0].value) 
   });
 });
+// the variable checkUsername is declared in the tests.js
--- tests.js
+++ tests.js
@@ -1,5 +1,31 @@
-const checkUsername = (user) => {
-  return /^[a-zA-Z]+$/.test(user)
+const checkUsername = (user) => {  // anonymous function, has no name. user is the parameter
+  return /^[a-zA-Z]+$/.test(user)   // pattern for a name, that can contain letter from a to z, small or capital, is not accepting number
+}
+
+const checkEmail = (email) => {     // email is the parameter
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(patter(\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)   // pattern for email
+}
+
+const checkPassword = (password) => {
+  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/.test(password)
 }
 
 console.log("username just letters: ",  checkUsername("Tommy"))
+console.log("valid email: ",  checkEmail("abc@abc.de"))  // ("abc@abc.de") is the argument
+console.log("check password: ",  checkPassword("Gede5?gfgf"))  
+
+
+
+
+
+
+/* function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email.toLowerCase());
+} */
+
+/* var regularExpression = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+Minimum eight characters, at least one letter, one number and one special character:
+
+
+ */
